7930ee51ce82806a31363c1af3cbfda7c730e0ce
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-11-20 12:18:37 +0300

    day-02 is ongoing...

--- day-02/ProductApp/Controllers/HomeController.cs
+++ day-02/ProductApp/Controllers/HomeController.cs
@@ -18,34 +18,7 @@ public class HomeController : Controller
         return View();
     }
 
-    // Create an action
-    public IActionResult GetOneProduct()
-    {
-        // 1. veriyi organize etmek 
-        var prd = new Product();
-        prd.Id = 1;
-        prd.ProductName = "Computer";
-        prd.Price = 17_000;
+   
 
-        // 2. view gönder 
-        return View("GetOneProduct",prd);
-
-
-    }
-
-    public IActionResult GetAllProducts()
-    {
-        var products = new List<Product>();
-        
-        products.Add(new Product() 
-        { 
-            Id=1, 
-            ProductName="Computer",
-            Price=17_000
-        });
-        products.Add(new Product(2, "Smart Phone", 15_000));
-        products.Add(new Product(3, "AirPods", 5_000));
-
-        return View(products);
-    }
+    
 }
