abe455e8443e9d5e317dbf6b8b7a7c02c58787ac
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-11-20 11:48:51 +0300

    HomeController has been defined.

---
+++ day-02/ProductApp/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ProductApp.Models;
+
+namespace ProductApp.Controllers;
+
+public class HomeController : Controller
+{
+
+    public IActionResult Index()
+    {
+        string msg = "Dünya erkek günü kutlu olsun.";
+        return View("Index",msg);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    // Create an action
+    public IActionResult GetOneProduct()
+    {
+        // 1. veriyi organize etmek 
+        var prd = new Product();
+        prd.Id = 1;
+        prd.ProductName = "Computer";
+        prd.Price = 17_000;
+
+        // 2. view gönder 
+        return View("GetOneProduct",prd);
+
+
+    }
+
+    public IActionResult GetAllProducts()
+    {
+        var products = new List<Product>();
+        
+        products.Add(new Product() 
+        { 
+            Id=1, 
+            ProductName="Computer",
+            Price=17_000
+        });
+        products.Add(new Product(2, "Smart Phone", 15_000));
+        products.Add(new Product(3, "AirPods", 5_000));
+
+        return View(products);
+    }
+}
---
+++ day-02/ProductApp/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿namespace ProductApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; } // property default : 0
+        public String ProductName { get; set; } // default : null
+        public Decimal Price { get; set; }  // default: 0
+
+        public Product()
+        {
+
+        }
+
+        public Product(int id, string productName, decimal price)
+        {
+            Id = id;
+            ProductName = productName;
+            Price = price;
+        }
+    }
+}
