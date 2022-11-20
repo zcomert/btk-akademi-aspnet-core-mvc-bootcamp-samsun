a59ebb7912f4fa5bd8b137d54d12768c50087095
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-11-20 15:26:53 +0300

    appsettings have been updated.

--- day-02/Repositories/EFCore/RepositoryContext.cs
+++ day-02/Repositories/EFCore/RepositoryContext.cs
@@ -1,14 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Repositories.EFCore
 {
     public class RepositoryContext : DbContext
     {
         public DbSet<Product> Products { get; set; }
+        public RepositoryContext(DbContextOptions<RepositoryContext> options)
+            : base(options)
+        {
+
+        }
     }
 }
