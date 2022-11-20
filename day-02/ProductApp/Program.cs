6c086f524dbfdd15508493be53c8c5dd56530369
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-11-20 15:54:42 +0300

    appsettings have been updated.

--- day-02/ProductApp/Program.cs
+++ day-02/ProductApp/Program.cs
@@ -1,7 +1,10 @@
+using Repositories.EFCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<RepositoryContext>();
 
 var app = builder.Build();
 
