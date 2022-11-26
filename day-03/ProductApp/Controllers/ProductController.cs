90bfddf0c57848e2cfdeb4e463902a535d64ba71
zcomert <zafer.comert@hotmail.com>
2022-11-26 20:52:50 +0300

    day-03 is ongoing...

---
+++ day-03/ProductApp/Controllers/ProductController.cs
@@ -0,0 +1,61 @@
+﻿using Entities.Models;
+using Microsoft.AspNetCore.Mvc;
+using Repositories.EFCore;
+
+namespace ProductApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly RepositoryContext _context;
+
+        public ProductController(RepositoryContext context)
+        {
+            _context = context;  
+        }
+
+        public IActionResult Index()
+        {
+            var products = _context.Products.ToList();
+            return View("Index",products);
+        }
+
+        public IActionResult GetOneProduct(int id)
+        {
+            var product = _context
+                .Products
+                .Where(p => p.Id.Equals(id))
+                .SingleOrDefault();
+
+            return View("GetOneProduct", product);
+        }
+
+        public IActionResult CreateOneProduct()
+        {
+            var product = new Product();
+            product.ProductName = "Glass";
+            product.Price = 2500;
+            product.ImageUrl = "/images/products/5.jpg";
+            product.Description = "For sunny days.";
+            product.AtCreated = DateTime.Now;
+
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult CreateOneProductWithView()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateOneProductWithView(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ day-03/Repositories/Migrations/20221126123106_init.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repositories.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true, defaultValue: "/images/products/default.jpg"),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true, defaultValue: "..."),
+                    AtCreated = table.Column<DateTime>(type: "datetime2", nullable: true, defaultValueSql: "GETDATE()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "ProductName" },
+                values: new object[] { 1, "HP Laptop Touch your Dreams", "/images/products/1.jpg", 15000m, "HP Z Book" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "ProductName" },
+                values: new object[] { 2, "Airpods for your ears", "/images/products/2.jpg", 5000m, "AirPods" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Price", "ProductName" },
+                values: new object[] { 3, 7000m, "Samsun Galaxy Note FE" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
