98d316e680b395521059c7a9031a95fac9dd9b76
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-11-27 15:12:15 +0300

    CategotyList has been done.

---
+++ day-04/ProductApp/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Repositories.EFCore;
+
+namespace ProductApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly RepositoryContext _context;
+
+        public CategoryController(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var categories = _context.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
