c89295d4edfb15c779dfe590361c5ead60c5a770
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-12-04 12:52:33 +0300

    Automapper has been defined.

--- day-06/Entities/DataTransferObjects/ProductForInsertionDto.cs
+++ day-06/Entities/DataTransferObjects/ProductForInsertionDto.cs
@@ -1,21 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Entities.DataTransferObjects
 {
-    public record ProductForInsertionDto
+
+    public record ProductForInsertionDto : ProductManipulationDto
     {
-        [Required(ErrorMessage = "Product Name is required.")]
-        public String? ProductName { get; init; } // default : null
         
-        [Required(ErrorMessage = "Price is required.")]
-        public Decimal Price { get; init; }  // default: 0
-        public String? ImageUrl { get; init; }
-        public String? Description { get; init; }
-        public int? CategoryId { get; init; }
     }
 }
