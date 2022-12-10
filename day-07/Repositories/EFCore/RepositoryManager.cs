1b5a19749f469003fc041d0e7808f11dcaf24052
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-12-10 16:17:54 +0300

    RepositoryManager has been implemented.

---
+++ day-07/Repositories/EFCore/RepositoryManager.cs
@@ -0,0 +1,34 @@
+﻿using Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.EFCore
+{
+    public class RepositoryManager : IRepositoryManager
+    {
+        private readonly RepositoryContext _context;
+        private IProductRepository _productRepository;
+        private ICategoryRepository _categoryRepository;
+
+        public RepositoryManager(RepositoryContext context, 
+            IProductRepository productRepository, 
+            ICategoryRepository categoryRepository)
+        {
+            _context = context;
+            _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public IProductRepository Product => _productRepository;
+
+        public ICategoryRepository Category => _categoryRepository;
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
