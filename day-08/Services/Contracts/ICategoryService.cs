1e49caf31e94f5110355daeca4478b224e256a8f
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-12-11 10:27:40 +0300

    Services has been added.

---
+++ day-08/Services/Contracts/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Contracts
+{
+    public interface ICategoryService
+    {
+        IEnumerable<Category> GetAllCategories();
+        Category GetOneCategoryById(int id);
+    }
+}
