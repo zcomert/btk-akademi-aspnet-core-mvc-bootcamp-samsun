51aa907043916923ca85e41fd59211bef0ddabbc
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-12-17 17:05:25 +0300

    Login and Register have been done.

--- day-09/ProductApp/Controllers/AuthenticationController.cs
+++ day-09/ProductApp/Controllers/AuthenticationController.cs
@@ -8,15 +8,44 @@ namespace ProductApp.Controllers
     public class AuthenticationController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public AuthenticationController(UserManager<ApplicationUser> userManager)
+        public AuthenticationController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login([FromForm] LoginDto model)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = await _userManager.FindByNameAsync(model.UserName);
+                if (user is not null)
+                {
+                    await _signInManager.SignOutAsync();
+                    var result = await _signInManager
+                        .PasswordSignInAsync(user, model.Password, false, false);
+
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Product");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("Error", "Username or password is invalid.");
+                    }
+                }
+            }
+
+            return View(model);
         }
 
         [HttpGet]
