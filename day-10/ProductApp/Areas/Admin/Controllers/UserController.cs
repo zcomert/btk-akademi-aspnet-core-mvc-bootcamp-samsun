6dab8aa8138e6d56e38c6152c30eadfc4a4f0a73
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-12-18 15:39:24 +0300

    Login and Register have been done.

--- day-10/ProductApp/Areas/Admin/Controllers/UserController.cs
+++ day-10/ProductApp/Areas/Admin/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using Entities.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace ProductApp.Areas.Admin.Controllers
@@ -29,7 +30,34 @@ namespace ProductApp.Areas.Admin.Controllers
             var user = await _userManager.FindByNameAsync(id);
             var roles = await _roleManager.Roles.ToListAsync();
             ViewBag.RoleList = roles;
+            ViewBag.Roles = new SelectList(roles, "Id", "Name");
             return View(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddRoleToUser(string username, string role)
+        {
+            var selectedRole = await _roleManager.FindByIdAsync(role);
+            var selectedUser = await _userManager.FindByNameAsync(username);
+            var flag = await _userManager.IsInRoleAsync(selectedUser, selectedRole.Name);
+
+            if (selectedRole is not null && selectedUser is not null && !flag)
+            {
+                var result = await _userManager
+                    .AddToRoleAsync(selectedUser, selectedRole.Name);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var err in result.Errors)
+                    {
+                        ModelState.AddModelError("", err.Description);
+                    }
+                }
+            }
+            return RedirectToAction("Index");
         }
     }
 }
