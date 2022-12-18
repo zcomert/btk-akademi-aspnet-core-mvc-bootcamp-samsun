ef90835d97eb193966e1fd35338a548ed4e2fa1b
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-12-18 12:22:33 +0300

    Login and Register have been done.

---
+++ day-10/ProductApp/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            return View(users);
+        }
+    }
+}
