9540ff7577535ceb766d4218effa5e30b7af5577
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-12-18 12:48:57 +0300

    Login and Register have been done.

--- day-10/ProductApp/Areas/Admin/Controllers/UserController.cs
+++ day-10/ProductApp/Areas/Admin/Controllers/UserController.cs
@@ -9,16 +9,27 @@ namespace ProductApp.Areas.Admin.Controllers
     public class UserController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public UserController(UserManager<ApplicationUser> userManager)
+        public UserController(UserManager<ApplicationUser> userManager, 
+            RoleManager<IdentityRole> roleManager)
         {
             _userManager = userManager;
+            _roleManager = roleManager;
         }
 
         public async Task<IActionResult> Index()
         {
             var users = await _userManager.Users.ToListAsync();
             return View(users);
+        }
+
+        public async Task<IActionResult> AddRoleToUser(string id)
+        {
+            var user = await _userManager.FindByNameAsync(id);
+            var roles = await _roleManager.Roles.ToListAsync();
+            ViewBag.RoleList = roles;
+            return View(user);
         }
     }
 }
